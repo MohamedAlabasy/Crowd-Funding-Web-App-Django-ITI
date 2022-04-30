ade9e488d3fe2870efd764c01958e62d6745e426
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-04-30 11:51:08 +0200

    finish get user projects

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,6 +1,29 @@
 from rest_framework import serializers
 from projects.models import Categories, Tags, Projects, Pictures, Comments, Replies, Reports, Donations
+from user.models import User
 
+#=======================================================================================#
+#			                            Categories                                     	#
+#=======================================================================================#
+
+
+class getCategories(serializers.ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = "__all__"
+
+#=======================================================================================#
+#			                               Tags                                     	#
+#=======================================================================================#
+
+
+class getTags(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = [
+            "id",
+            "name"
+        ]
 #=======================================================================================#
 #			                            0                                     	#
 #=======================================================================================#
