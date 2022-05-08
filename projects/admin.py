6002b390e85be77dd6704d94c493a2af3b044f56
dinareda <dina.reda.elsebaey@gmail.com>
2022-05-09 01:37:15 +0200

    latestAdminSelected

--- projects/admin.py
+++ projects/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Categories, Tags, Projects, Pictures, Comments, Replies, ReportsComment, ReportsProject, Donations, Rates
+from .models import Projects, Categories, Tags, Projects, Pictures, Comments, Replies, ReportsComment, ReportsProject, Donations, Rates
 
 #=======================================================================================#
 #			                           Register models                                  #
 #=======================================================================================#
 admin.site.register(Categories)
 admin.site.register(Tags)
-admin.site.register(Projects)
+# admin.site.register(Projects)
 admin.site.register(Pictures)
 admin.site.register(Comments)
 admin.site.register(Replies)
@@ -14,3 +14,9 @@ admin.site.register(ReportsProject)
 admin.site.register(ReportsComment)
 admin.site.register(Donations)
 admin.site.register(Rates)
+
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display =('title','category','created_at')
+    ordering=('title',)
+    search_fields=('title','category')
